f49cf5d6cc924a7239d024b2973a3bcf14122187
Mayank <mayank@wedigtech.com>
2019-04-16 18:28:17 +0530

    Payment-option page integrated

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -17,12 +17,12 @@
   // Local Config
 
   //var  apiUrl='http://192.168.1.156:3002/'; //local API
-  var siteUrl="http://192.168.1.110:4200"; // local site url
+  //var siteUrl="http://192.168.1.110:4200"; // local site url
   // var apiUrl='http://192.168.1.156:3003/'; //local soa
  
  export const environment = {
   production: false,
-  adimageUrl:AdminUrl+"ads/3/",
+  adimageUrl:AdminUrl+"ads",
   missionImage:AdminUrl+"edit_message",
   importantLinksIcones:AdminUrl+"link_icon",
   apiUrl:apiUrl,
@@ -34,7 +34,8 @@
   siteUrl:siteUrl,
   cms_img:AdminUrl+"/cms_img/",
   download:AdminUrl+"download/",
-  payment_options:"1_dw4-toj_9myfuywld3qzja_1555311365.png",
+  payment_options:AdminUrl+"other/",
+
   ads:{
     options:{
     pubId: "pub-9616389000213823", // Make sure this the correct client ID!
